acf836194da46e3eae6d33192b41df3680ea5a37
Ricky Aguayo <raguayo911@gmail.com>
2018-02-10 20:56:08 -0600

    more work

--- take 2/scripts/main.js
+++ take 2/scripts/main.js
@@ -1,27 +1,45 @@
-var words = ["fun", "exciting", "about not giving up", "being helpful", "being open", "what I learned at CodingDojo!"];
-var el = document.getElementById("titles");
-var word_counter = 0;
-var character_counter = 0;
+// var str = "FirstLine\n\\Second Line\\\rThirdLine"
 
-function updateText() {
-	if(words[word_counter][character_counter] == ' '){
-		el.innerHTML = el.innerHTML + '&nbsp;'
-		character_counter++;
-	} else {
-		el.innerHTML = el.innerHTML + words[word_counter][character_counter++];
-	}
+// console.log(str);
 
-	if(character_counter == words[word_counter].length + 1){
-		word_counter++; 	//choose a different word
-		character_counter = 0;	//start over with the first character of the word
-		el.innerHTML = '';  //set the html to be blank
+// var lastName = "Aguayo"
 
-		//if we're displaying the last word, go back to the first word
-		if(word_counter == words.length)
-			word_counter = 0;
-	}
+// console.log(lastName[lastName.length - 1])
+
+// function reuseableFunction() {
+//   console.log("Hi World");
+// }
+
+// reuseableFunction();
+
+
+// Setup
+// var outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   // Only change code below this line
+//   var outerWear = "sweater"
+  
+//   console.log(outerWear);
+//   // Only change code above this line
+//   return outerWear;
+// }
+
+// console.log(outerWear);
+
+// function timesFive(num) {
+// 	return num * 5;
+// }
+
+// console.log(timesFive(4));
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  arr.shift();
+  
+  return arr;  // Change this line
 }
 
-document.onload = function(){
-	setInterval(updateText, 200);
-}
+console.log(nextInLine([1,2,3,4], 5))
+
