75e7a5bce974e4c8a8274afc90bc25d452779532
Ricky Aguayo <raguayo911@gmail.com>
2018-02-28 14:55:36 -0600

    adding more

--- take 2/scripts/main.js
+++ take 2/scripts/main.js
@@ -1,11 +1,24 @@
-var i = 0;
-var words = ["am a web developer", "freelance", "am probably hungry"];
-var speed = 50;
+var words = ["am a web developer", "freelance", "love to code", "am probably hungry"];
+var el = document.getElementById('phrases');
+var word_counter = 0;
+var char_counter = 0;
 
 function typeWriter(){
-	if(i < words.length){
-		document.getElementById("phrases").innerHTML += words.charAt(i);
-		i++;
-		setTimeout(typeWriter, speed);
+	if(words[word_counter][char_counter] == ' '){
+		el.innerHTML = el.innerHTML + '&nbsp;'
+		character_counter++;
+	} else {
+		el.innerHTML = el.innerHTML + words[word_counter][char_counter++];
+	}
+	if(char_counter == words[word_counter].length + 1) {
+		word_counter++; 	//choose a different word
+		character_counter = 0;	//start over with the first character of the word
+		el.innerHTML = '';  //set the html to be blank
+
+	//if we're displaying the last word, go back to the first word
+	if(word_counter == words.length) 
+		word_counter = 0;
 	}
 }
+
+setInterval(typeWriter, 200);
